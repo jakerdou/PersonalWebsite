198ec6569fa8cfad8c328b4443692535dbafec38
James Robinson <44409757+jakerdou@users.noreply.github.com>
2020-04-04 15:07:49 -0500

    adding test html

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './component/App';
-import ContentTabs from './component/ContentTabs';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><App /></Router>, document.getElementById('root'))
